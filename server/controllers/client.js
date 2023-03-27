50cb6e82441b1d6c182736a37478db892d1ff482
Matthew Bronstein <mbronstein1@gmail.com>
2023-03-27 16:57:37 -0500

    added products route on backend

---
+++ server/controllers/client.js
@@ -0,0 +1,31 @@
+const { Product } = require('../models');
+const { ProductStat } = require('../models');
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    if (!products) {
+      return res.status(400).json({ message: 'Something went wrong!' });
+    }
+
+    // If the product stats were set as a reference to the products in the data,
+    // this step would be unnecessary because we could just say .populate to display all
+    //connected product stats to their referenced products
+    const productsWithStats = await Promise.all(
+      products.map(async product => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+
+    res.status(200).json(productsWithStats);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,4 +1,7 @@
 // Route = '/client'
 const router = require('express').Router();
+const { getProducts } = require('../controllers/index');
+
+router.get('/products', getProducts);
 
 module.exports = router;
